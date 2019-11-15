a1ee95928b479bd9ca99aa4c95a386131406981b
Dora Chua <msdorachua@users.noreply.github.com>
2019-11-15 21:45:04 +0800

    Update app.py

--- app.py
+++ app.py
@@ -15,3 +15,9 @@ def hello_user(username):
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')  # open for everyone
+    ############# Add these lines #############
+    import xmlrunner
+    runner = xmlrunner.XMLTestRunner(output='test-reports')
+    unittest.main(testRunner=runner)
+    ###########################################
+    unittest.main()
